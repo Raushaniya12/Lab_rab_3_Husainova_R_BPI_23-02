a32a8432a8cc6e914dbb143b5ed1aa8440b2e374
R.Z. Husainova <rausaniahusainova724@gmail.com>
2026-04-05 22:06:38 +0500

    .

--- Lab_rab_3_Husainova_R_BPI_23-02/App_Start/RouteConfig.cs
+++ Lab_rab_3_Husainova_R_BPI_23-02/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace Lab_rab_3_Husainova_R_BPI_23_02
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+             name: "Default",
+             url: "{controller}/{action}/{id}",
+             defaults: new
+             {
+                 controller = " VegetablesController ",
+                 action = "FirstViewMethod",
+                 id =
+            UrlParameter.Optional
+             }
             );
         }
     }
