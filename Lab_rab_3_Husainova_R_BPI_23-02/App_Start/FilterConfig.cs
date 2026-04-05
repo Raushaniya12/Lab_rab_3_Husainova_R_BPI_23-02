47b6516883c7fd7fc5d5256c9977793f54633fb9
R.Z. Husainova <rausaniahusainova724@gmail.com>
2026-04-05 21:33:30 +0500

    Добавьте файлы проекта.

---
+++ Lab_rab_3_Husainova_R_BPI_23-02/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_rab_3_Husainova_R_BPI_23_02
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lab_rab_3_Husainova_R_BPI_23-02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab_rab_3_Husainova_R_BPI_23_02.MvcApplication" Language="C#" %>
