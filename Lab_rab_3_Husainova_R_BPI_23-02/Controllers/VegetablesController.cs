f389fde7d000d360a46bac44905bffd9d543a646
R.Z. Husainova <rausaniahusainova724@gmail.com>
2026-04-05 21:49:00 +0500

    .

---
+++ Lab_rab_3_Husainova_R_BPI_23-02/Controllers/VegetablesController.cs
@@ -0,0 +1,27 @@
+﻿using Lab_rab_3_Husainova_R_BPI_23_02.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_rab_3_Husainova_R_BPI_23_02.Controllers
+{
+    public class VegetablesController : Controller
+    {
+        public ActionResult FirstViewMethod()
+        {
+            // Создаем список овощей
+            List<Vegetable> veggies = new List<Vegetable>
+             {
+                 new Vegetable { Id = 1, Name = "Картофель" },
+                 new Vegetable { Id = 2, Name = "Морковь" },
+                 new Vegetable { Id = 3, Name = "Лук" },
+                 new Vegetable { Id = 4, Name = "Капуста" },
+                 new Vegetable { Id = 5, Name = "Свекла" }
+             };
+            // Передаем список в представление в качестве модели
+            return View(veggies);
+        }
+    }
+}
---
+++ Lab_rab_3_Husainova_R_BPI_23-02/Models/Vegetable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_rab_3_Husainova_R_BPI_23_02.Models
+{
+    public class Vegetable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
